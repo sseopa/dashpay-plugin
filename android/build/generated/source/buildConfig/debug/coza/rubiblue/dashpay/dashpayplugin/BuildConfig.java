0128f9e9be3bf256be43733104ca3012ec130f89
sethake <sseopa@rubiblue.co.za>
2021-04-06 18:39:54 +0200

    added aditional resources

---
+++ android/build/generated/source/buildConfig/debug/coza/rubiblue/dashpay/dashpayplugin/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package coza.rubiblue.dashpay.dashpayplugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "coza.rubiblue.dashpay.dashpayplugin";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "coza.rubiblue.dashpay.dashpayplugin";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
