258341ccb84fda9b9e8bd48e2586b1029334d297
sethake <sseopa@rubiblue.co.za>
2021-05-10 21:47:50 +0200

    updates to plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -7,5 +7,5 @@ declare module '@capacitor/core' {
 export interface DashPayModulePlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
   getSerial(options: { value: string }): Promise<{ value: string }>;
-  print(printString: string,EXTRA_ORIGINATING_URI:string,dashpaypackagename:string): Promise<{results: any[]}>;
+  print(options:{printString: string,EXTRA_ORIGINATING_URI:string}): Promise<{results: any[]}>;
 }
--- src/web.ts
+++ src/web.ts
@@ -17,10 +17,10 @@ export class DashPayModuleWeb extends WebPlugin implements DashPayModulePlugin {
     console.log('getSerial', options);
     return options;
   }
-  async print(printString: string,EXTRA_ORIGINATING_URI:string,dashpaypackagename:string): Promise<{ results: any[] }> {
-    console.log('print string: ', printString);
-    console.log('EXTRA_ORIGINATING_URI: ', EXTRA_ORIGINATING_URI);
-    console.log('dashpaypackagename: ', dashpaypackagename);
+  async print(options:{printString: string,EXTRA_ORIGINATING_URI:string}): Promise<{ results: any[] }> {
+    console.log('print string: ', options.printString);
+    console.log('EXTRA_ORIGINATING_URI: ', options.EXTRA_ORIGINATING_URI);
+    
     return {
       results: [{
         firstName: 'Dummy',
