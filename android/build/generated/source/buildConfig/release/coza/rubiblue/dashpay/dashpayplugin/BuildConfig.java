808eb63c8f1e61d28db98039e90220af775f81d9
Kgaugelo Ledwaba <kledwaba@rubiblue.co.za>
2023-03-10 10:47:42 +0200

    Sync

---
+++ android/build/generated/source/buildConfig/release/coza/rubiblue/dashpay/dashpayplugin/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package coza.rubiblue.dashpay.dashpayplugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "coza.rubiblue.dashpay.dashpayplugin";
+  public static final String BUILD_TYPE = "release";
+}
